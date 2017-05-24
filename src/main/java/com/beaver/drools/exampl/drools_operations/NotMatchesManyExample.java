749ef19ac19357cc9035d9ec23b5f1e445cfe771
sdcuike <sdcuike@aol.com>
2017-05-24 16:40:50 +0800

    NotMatchesManyExample

---
+++ src/main/java/com/beaver/drools/exampl/drools_operations/NotMatchesManyExample.java
@@ -0,0 +1,26 @@
+package com.beaver.drools.exampl.drools_operations;
+
+import com.beaver.drools.util.KieSessionUtil;
+import org.kie.api.runtime.StatelessKieSession;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by beaver on 2017/5/24.
+ */
+public class NotMatchesManyExample {
+    
+    public static void main(String[] args) {
+        KieSessionUtil kieSessionUtil = new KieSessionUtil();
+        kieSessionUtil.addFromClassPath("/rules/drools_operations/NotMatchesManyExample.drl", StandardCharsets.UTF_8.name());
+        
+        kieSessionUtil.verifyRules();
+        StatelessKieSession statelessKieSession = kieSessionUtil.build().newStatelessKieSession();
+        
+        String str = "sCNN000020000";
+        
+        boolean matches = str.matches("^(22|121a).*");
+        System.out.println(matches);
+        statelessKieSession.execute(str);
+    }
+}
